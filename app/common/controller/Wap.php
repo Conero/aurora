56e01181a6c7479ff302ea07f6e7dae206e4ef24
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-06 08:42:58 +0000

    v1.0.1

---
+++ app/common/controller/Wap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Joshua Conero
+ * Date: 2017/5/6 0006 12:13
+ * Email: brximl@163.com
+ * Name: 移动端公共控制器
+ */
+namespace app\common\controller;
+use think\Controller;
+
+class Wap extends Controller
+{
+
+}
--- app/wap/controller/Index.php
+++ app/wap/controller/Index.php
@@ -1,19 +1,36 @@
 <?php
-namespace app\index\controller;
+/**
+ * User: Joshua Conero
+ * Date: 2017/5/6 0006 12:13
+ * Email: brximl@163.com
+ * Name: 移动端首页
+ */
 
-class Index
+namespace app\wap\controller;
+use app\common\controller\Wap;
+class Index extends Wap
 {
     public function index()
     {
         /*
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
         */
+        /*
         return '
             <!doctype html>
             <head>
+                <meta charset="utf-8">
+                <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0,  user-scalable=0" name="viewport" />
                 <title>Aurora - Joshua Coneoro</title>
-                <link rel="shortcut icon" href="/aurora/public/aurora.ico"/>
-                <link rel="shortcut icon" href="/aurora/public/aurora.ico"/>
+                <link rel="shortcut icon" href="/aurora/public/aurora-appm.ico"/>
+                <link rel="shortcut icon" href="/aurora/public/aurora-appm.ico"/>
+
+
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+                <title>WeUI</title>
+                <link rel="stylesheet" href="../style/weui.css"/>
+                <link rel="stylesheet" href="./example.css"/>
             </head>
             <body>
                 <div style="margin-left:5%;margin-right:5%;">
@@ -25,5 +42,7 @@ class Index
             </body>
             </html>
         ';
+        */
+        return $this->fetch();
     }
 }
