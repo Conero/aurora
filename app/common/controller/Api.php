49a2e09e797421352a53f739437266f1812d74ae
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-14 00:42:55 +0000

    系统优化，以及api自动登记

--- app/common/controller/Api.php
+++ app/common/controller/Api.php
@@ -9,13 +9,21 @@
 namespace app\common\controller;
 use app\common\traits\DbUtil;
 use app\common\traits\Util;
+use think\Config;
 use think\Controller;
+use think\Request;
+use think\Session;
 
 class Api extends Controller
 {
     use DbUtil; // 数据库助手
     use Util;   // 公共方法， Web, Api, Wap
-
+    protected function _initialize(){
+        $this->api_check();
+        $this->autoRecordApis();
+        $this->init();
+    }
+    protected function init(){}    // 应用初始化接口
     /**
      * 信息反馈
      * @param $msg
@@ -28,5 +36,55 @@ class Api extends Controller
             'msg' => $msg
         ];
         return json($data);
+    }
+
+    /**
+     * 自动登记或同级api请求数据
+     */
+    protected function autoRecordApis(){
+        $request = Request::instance();
+        $module = $request->module();
+        $contrl = $request->controller();
+        $action = $request->action();
+        $url = Config::get('setting.url_pref').$module."/$contrl/$action";
+        $api = model('Apis');
+        $data = $api->where('url',$url)->field('listid,count')->find();
+        if($data){
+            $listId = $data['listid'];
+            $count = intval($data['count']) + 1;
+            $api->update(['count'=>$count],['listid'=>$listId]);
+        }else{
+            $uid = getUserInfo('uid');
+            $saveData = [
+                'url' => $url,
+                'count'=>1,
+                'module'=>$module,
+                'controller'=>$contrl,
+                'action'=>$action
+            ];
+            if($uid) $saveData['uid'] = $uid;
+            $api->insert($saveData);
+        }
+    }
+
+    /**
+     * api 请求验证
+     */
+    protected function api_check(){
+        /*
+        $skey = Config::get('setting.session_api_sfkey');
+        $svalue = Session::get($skey);
+        if(empty($svalue)){
+            Session::set($skey,sha1(request()->ip()));
+            $svalue = Session::get($skey);
+        }
+        if(empty($svalue)){
+            header('content-type:application/json;charset=utf-8;');
+            die(json_encode([
+                "code"=>-1,
+                "msg"=>"非法请求地址"
+            ]));
+        }
+        */
     }
 }
--- app/extra/setting.php
+++ app/extra/setting.php
@@ -11,6 +11,7 @@ return [
     'session_scounter_key' => 'aurora_sct_key',    // 系统计数器session值
     'session_cache'     => 'aurora_sckey',          // sesion 缓存键值
     'session_cache_dir'  => 'runtime/_scache/',         // session_cache 地址
+    'session_api_sfkey'  => 'ara_sfkey',                // session API 安全机制机制处理
 
     'gzh_code'      => '5400',  // 公众号
     'gzh_code_debug'=> 1,       // 公众号是否开启调试
@@ -18,7 +19,7 @@ return [
     // 用于底部版权
     'organization' => 'Coenro',    // 机构组织， 用于底部才菜单
     'start'         => '2014',    // 起始时间
-    'version'       => 'V 0.1.0',          // 版本号
+    'version'       => '0.1.0',          // 版本号
     'build'         => '20170514',       // 更新时间
     'online_date'   => '2017-05-05',                  // 上线时间
 
@@ -27,5 +28,6 @@ return [
     
     // 前端设置
     'static_pref'=>'/aurora/public/',
+    'url_pref'  => '/aurora/',      // 地址前缀
     'debug_dir' => '/runtime/'
 ];
