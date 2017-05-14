81a85efb6054839389d3cf7d1485964cfe16d29e
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-14 00:42:55 +0000

    系统优化，以及api自动登记

---
+++ app/common/model/Apis.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Auther: Joshua Conero
+ * Date: 2017/5/14 0014 8:13
+ * Email: brximl@163.com
+ * Name:
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class Apis extends Model
+{
+    protected $pk = 'listid';
+}
