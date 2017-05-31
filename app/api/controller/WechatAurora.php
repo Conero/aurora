75bb58a5284638b0ec561733ac55bc2bf911c6ac
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-31 13:45:28 +0000

    订阅号后台服务查询

--- app/api/controller/WechatAurora.php
+++ app/api/controller/WechatAurora.php
@@ -6,15 +6,21 @@
  * Email: brximl@163.com
  * Name: 微信服务实现
  */
+use app\common\model\Loger;
 import('wechat-php-sdk.src.Wechat',EXTEND_PATH);
 class WechatAurora extends Wechat
 {
+    private $LogCode = 'weixin_dyh_log';
     /**
      * 用户关注时触发，回复「欢迎关注」
      *
      * @return void
      */
     protected function onSubscribe() {
+        $log = new Loger();
+        $content = request()->ip().
+            "用于关注订阅号，关注时触发接口!";
+        $log->write($this->LogCode,$content);
         $this->responseText('欢迎关注,生成测试时');
     }
 
@@ -24,6 +30,10 @@ class WechatAurora extends Wechat
      * @return void
      */
     protected function onUnsubscribe() {
+        $log = new Loger();
+        $content = request()->ip().
+            ">用户取消关注时触发";
+        $log->write($this->LogCode,$content);
         // 「悄悄的我走了，正如我悄悄的来；我挥一挥衣袖，不带走一片云彩。」
     }
 
@@ -33,6 +43,10 @@ class WechatAurora extends Wechat
      * @return void
      */
     protected function onText() {
+        $log = new Loger();
+        $content = request()->ip().
+            ">收到文本消息时触发，回复收到的文本消息内容";
+        $log->write($this->LogCode,$content);
         $this->responseText('收到了文字消息：' . $this->getRequest('content'));
     }
 }
