466076a7ba4358ca76616f9992ad98e1b57c3cc5
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-31 15:25:23 +0000

    v0.1.16

--- app/api/controller/WechatAurora.php
+++ app/api/controller/WechatAurora.php
@@ -7,13 +7,13 @@
  * Name: 微信服务实现
  */
 use app\common\model\Loger;
+use app\common\model\Prj1001c;
 import('wechat-php-sdk.src.Wechat',EXTEND_PATH);
 class WechatAurora extends Wechat
 {
     public $LogCode = 'weixin_dyh_log';
     /**
      * 用户关注时触发，回复「欢迎关注」
-     *
      * @return void
      */
     protected function onSubscribe() {
@@ -49,14 +49,73 @@ class WechatAurora extends Wechat
      * @return void
      */
     protected function onText() {
+        $text = trim($this->getRequest('content'));
         $log = new Loger();
         $content = request()->ip()
             .">收到文本消息时触发，回复收到的文本消息内容"
+            ."\r\n文本内容：\r\n"
+            .$text
             ."\r\n请求数据：\r\n"
-            . json_encode(request()->param())
+            .print_r(request()->param(),true)
+            //. json_encode(request()->param())
         ;
         $log->write($this->LogCode,$content);
-        $this->responseText('收到了文字消息：' . $this->getRequest('content'));
+        $cmdList = [
+            'zw:' => '搜索文章列表'
+        ];
+        foreach ($cmdList as $k=>$v){
+            if(substr_count($text,$k)>0){
+                $msg = '';
+                switch ($k){
+                    case 'zw:':
+                        $where = [];
+                        $childList = ['a','wj'];
+                        foreach ($childList as $vv){
+                            if(substr_count($text,$k.$vv)>0){
+                                $value = trim(str_replace($k.$vv,'',$text));
+                                switch ($vv){
+                                    case 'a':
+                                        $where = ['sign'=>['like',"%$value%"]];
+                                        break;
+                                    case 'wj':
+                                        $where = ['collected'=>['like',"%$value%"]];
+                                        break;
+                                }
+                                break;
+                            }
+                        }
+                        if(empty($where)) {
+                            $value = trim(str_replace($k,'',$text));
+                            $where = ['title' => ['like',"%$value%"]];
+                        };
+                        $data = db()->table('atc1000c')
+                            ->field('listid,title,sign,collected,date')
+                            ->where($where)
+                            ->limit(30)
+                            ->select()
+                        ;
+                        foreach ($data as $dt){
+                            $msg .= '
+                            <li><a href="'.(\think\Config::get('setting.p_baseurl')).'wap/article/read/item/'.$dt['listid'].'.html">
+                            '.$dt['title'].'('.$dt['collected'].').'.$dt['sign'].' - '.$dt['date'].'
+                            </a></li>
+                            ';
+                        }
+                        $msg = $msg? '<ul>'.$msg.'</ul>':'没有找到资源，sorry，guys!';
+                        break;
+                }
+                if($msg){
+                    $this->responseText($msg);return;
+                }
+                break;
+            }
+        }
+        // 输入文章文本
+        $this->responseText(
+            (new Prj1001c())->getSetVal('weixin_api.cmd_list_help','Jessica',true)
+        );
+        return;
+        //$this->responseText('收到了文字消息：' . $this->getRequest('content'));
     }
 
     /**
