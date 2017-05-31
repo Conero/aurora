440c6a14cfe21cdd21481f5234a5f1a9faf4003d
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-05-31 13:56:42 +0000

    微信公众号请求日志

--- app/api/controller/WechatAurora.php
+++ app/api/controller/WechatAurora.php
@@ -10,7 +10,7 @@ use app\common\model\Loger;
 import('wechat-php-sdk.src.Wechat',EXTEND_PATH);
 class WechatAurora extends Wechat
 {
-    private $LogCode = 'weixin_dyh_log';
+    protected $LogCode = 'weixin_dyh_log';
     /**
      * 用户关注时触发，回复「欢迎关注」
      *
@@ -18,8 +18,11 @@ class WechatAurora extends Wechat
      */
     protected function onSubscribe() {
         $log = new Loger();
-        $content = request()->ip().
-            "用于关注订阅号，关注时触发接口!";
+        $content = request()->ip()
+            ."用于关注订阅号，关注时触发接口!"
+            ."\r\n请求数据：\r\n"
+            . json_encode(request()->param())
+        ;
         $log->write($this->LogCode,$content);
         $this->responseText('欢迎关注,生成测试时');
     }
@@ -31,8 +34,11 @@ class WechatAurora extends Wechat
      */
     protected function onUnsubscribe() {
         $log = new Loger();
-        $content = request()->ip().
-            ">用户取消关注时触发";
+        $content = request()->ip()
+            .">用户取消关注时触发"
+            ."\r\n请求数据：\r\n"
+            . json_encode(request()->param())
+        ;
         $log->write($this->LogCode,$content);
         // 「悄悄的我走了，正如我悄悄的来；我挥一挥衣袖，不带走一片云彩。」
     }
@@ -44,9 +50,44 @@ class WechatAurora extends Wechat
      */
     protected function onText() {
         $log = new Loger();
-        $content = request()->ip().
-            ">收到文本消息时触发，回复收到的文本消息内容";
+        $content = request()->ip()
+            .">收到文本消息时触发，回复收到的文本消息内容"
+            ."\r\n请求数据：\r\n"
+            . json_encode(request()->param())
+        ;
         $log->write($this->LogCode,$content);
         $this->responseText('收到了文字消息：' . $this->getRequest('content'));
+    }
+
+    /**
+     * 收到链接消息时触发，回复收到的链接地址
+     *
+     * @return void
+     */
+    protected function onLink() {
+        $log = new Loger();
+        $content = request()->ip()
+            .">收到链接消息时触发，回复收到的链接地址"
+            ."\r\n请求数据：\r\n"
+            . json_encode(request()->param())
+        ;
+        $log->write($this->LogCode,$content);
+        $this->responseText('收到了链接：' . $this->getRequest('url'));
+    }
+
+    /**
+     * 收到自定义菜单消息时触发，回复菜单的EventKey
+     *
+     * @return void
+     */
+    protected function onClick() {
+        $log = new Loger();
+        $content = request()->ip()
+            .">收到自定义菜单消息时触发，回复菜单的EventKey"
+            ."\r\n请求数据：\r\n"
+            . json_encode(request()->param())
+        ;
+        $log->write($this->LogCode,$content);
+        $this->responseText('你点击了菜单：' . $this->getRequest('EventKey'));
     }
 }
