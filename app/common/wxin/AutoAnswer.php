9638b4fc17942dae57c792e477d0b77eb77481d1
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-06-01 00:58:24 +0000

    v0.1.17(1)

---
+++ app/common/wxin/AutoAnswer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Auther: Joshua Conero
+ * Date: 2017/5/31 0031 23:42
+ * Email: brximl@163.com
+ * Name: 微信自动复数，解析发送的文本信息
+ */
+
+namespace app\common\wxin;
+
+
+class AutoAnswer
+{
+    public function __construct($text=null)
+    {
+    }
+}
