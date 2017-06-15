1927974a6f60b8e1be1d25f0f9bace9f40143d6a
Joshua Conero <Joshua Conero@a39be781-dba6-4fbb-a9d1-408a5d658220>
2017-06-15 16:02:25 +0000

    0.2.2

---
+++ app/common/model/Fnc0020c.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Auther: Joshua Conero
+ * Date: 2017/6/15 0015 23:28
+ * Email: brximl@163.com
+ * Name:
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class Fnc0020c extends Model
+{
+    protected $pk = 'listid';
+    protected $table = 'fnc0020c';
+}
--- app/wap/controller/Faffair.php
+++ app/wap/controller/Faffair.php
@@ -10,16 +10,48 @@ namespace app\wap\controller;
 
 
 use app\common\controller\Wap;
+use app\common\model\Fnc0020c;
+use hyang\Bootstrap;
 
 class Faffair extends Wap
 {
     public function index(){
+        $this->checkAuth();
+        $uid = $this->getUserInfo('uid');
+        $fnc20 = new Fnc0020c();
+        $data = $fnc20->where('uid',$uid)
+            ->order('mtime')
+            ->select();
+        $listCtt = '';
+        foreach ($data as $v){
+            $listCtt .= '
+            <a class="weui-cell weui-cell_access" href="'.url('faffair/edit','uid='.$v['listid']).'">
+                <div class="weui-cell__bd">
+                    <p>'.$v['name'].'</p>
+                </div>
+                <div class="weui-cell__ft"></div>
+            </a>
+            ';
+        }
+        if($listCtt) $this->assign('listCtt',$listCtt);
         return $this->fetch();
     }
     public function edit(){
+        $this->checkAuth();
         $this->loadScript([
             'js' => 'faffair/edit'
         ]);
+        $listid = request()->param('uid');
+        $uid = $this->getUserInfo('uid');
+        if($listid){
+            $fnc20x = new Fnc0020c();
+            $data = $fnc20x->get($listid)->toArray();
+            if($data['uid'] != $uid){
+                $this->getErrorUrl('请求参数有误!');
+            }
+            $this->assign('data',$data);
+            $this->assign('pk_form',Bootstrap::formPkGrid($data));
+        }
         return $this->fetch();
     }
 }
